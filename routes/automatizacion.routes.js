94f54c60da685e1b9e8b89deac47f451e8267197
briansrt <brianstevenriofriotrochez@hotmail.com>
2026-04-01 11:46:36 -0500

    routes buenas

--- routes/automatizacion.routes.js
+++ routes/automatizacion.routes.js
@@ -1,17 +1,14 @@
 import express from "express";
-import { CrearPQRSCoosalud, ConsultarRadicadoPQRS, enviarCorreosDesdeExcel, enviarTemplateWP, confirmarCitaHSMInicial, confirmarCitaHSM, enviarTemplateContactarPrestadorWP } from "../controllers/automatizaciones/coosalud/automatizacion.js";
+import { CrearPQRSCoosalud, ConsultarRadicadoPQRS, enviarCorreosDesdeExcel } from "../controllers/automatizaciones/coosalud/automatizacion.js";
 import { AutorizacionGuajira, ConsultarAutorizacion, AgendarCitaGuajiraCristal, ReAgendarCitaGuajiraCristal, CancelarCitaGuajiraCristal, VerificarAsistenciaCitaCristal } from "../controllers/automatizaciones/guajira/automatizacion.js";
-import { descargarAutorizacion, enviarCorreoCitaEndpoint } from "../controllers/automatizaciones/famisanar/automatizacion.js";
+import { descargarAutorizacion } from "../controllers/automatizaciones/famisanar/automatizacion.js";
 import { AutorizacionColpatria, AutorizacionEnfaso } from "../controllers/automatizaciones/enfaso/automatizacion.js";
-import { enviarCorreoComercial } from "../controllers/automatizaciones/comercial/automatizacion.js";
-import { autorizacionAgenteFamisanar } from "../controllers/automatizaciones/agents/automatizacion.js";
 
 const router = express.Router();
 
 router.post("/autorizacionesFamisanar", descargarAutorizacion);
 router.post("/autorizacionEnfaso", AutorizacionEnfaso);
 router.post("/autorizacionColpatria", AutorizacionColpatria)
-router.post("/enviarCorreoConfirmacion", enviarCorreoCitaEndpoint)
 
 router.post("/autorizacionGuajira", AutorizacionGuajira)
 router.post('/consultar', ConsultarAutorizacion);
@@ -23,11 +20,5 @@ router.post("/verificarCitaCumplida", VerificarAsistenciaCitaCristal)
 router.post("/crearPQRSCoosalud", CrearPQRSCoosalud)
 router.post("/consultarRadicadoPQRS", ConsultarRadicadoPQRS)
 router.post("/enviarCorreo", enviarCorreosDesdeExcel);
-router.post("/actualizarHSM", confirmarCitaHSM)
-router.post("/enviarNotificacionTemplate", enviarTemplateContactarPrestadorWP)
-router.post("/enviarTemplate", enviarTemplateWP)
 
-router.post("/enviarCorreoComercial", enviarCorreoComercial);
-
-router.post("/pruebaAgent", autorizacionAgenteFamisanar)
 export default router;
